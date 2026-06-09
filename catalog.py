6efa9b605ecd8862c2fcc8a35e1d279954a6fcf9
Столбовой В.И. <vadikstolbovoy@gmail.com>
2026-06-09 19:35:35 +0300

    feet: добавить модуль каталога(Product, Catalog, Warehouse)

---
+++ catalog.py
@@ -0,0 +1,36 @@
+
+class Produst:
+
+    def __init__(self, id, name, description, price, category):
+        self.id = id
+        self.name = name
+        self.description = description, price, category
+    
+    def update_data(self):
+        pass
+
+class Catalog:
+    
+    def __init__(self, name):
+        self.name = name
+    def.sort(self):
+        pass
+    deef filter(self):
+        pass
+    def filter(self):
+        pass
+    def shoe_product(self):
+       pass
+
+class Warehouse:
+   
+    def __init__(self, code, address):
+        self.code = code
+        self.address = address
+    def check_avilability(self):
+        pass
+    def collect_order(self):
+        pass
+    def write_off_product(self):
+        pass
+
